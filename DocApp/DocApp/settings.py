e059cf3ac3f34bf715b2eb2f26f21d7e3c4d7227
Fariza17 <Fariza17@users.noreply.github.com>
2021-02-20 17:03:16 +0300

    Created the superuser

--- DocApp/DocApp/settings.py
+++ DocApp/DocApp/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'DocApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'keeiynnl',
+        'HOST' : 'dumbo.db.elephantsql.com',
+        'USER' : 'keeiynnl',
+        'PASSWORD' : 'UDyp15hCiDMTr3LtG--sRWMVTKgsWLtO',
+        'PORT': '5432'
     }
 }
 
